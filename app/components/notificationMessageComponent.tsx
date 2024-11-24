2704fa000f78e17a2d28d1cc6c9689f1b02a9a29
Eyad Aimen <70057337+EyadAimen@users.noreply.github.com>
2024-11-24 21:57:40 +0800

    completed the notifiaction page design

---
+++ app/components/notificationMessageComponent.tsx
@@ -0,0 +1,44 @@
+import { Text, View, StyleSheet } from "react-native"
+import { theme } from "../../theme"
+
+type props = {
+    date: string,
+    status: string,
+    statusMessage: string,
+    message: string,
+}
+
+export function NotificationMessageComponent({ date, status, statusMessage, message }: props) {
+    return(
+    <View style={styles.card}>
+        <Text 
+        style={styles.dateText}>{date}</Text>
+        <Text style={[styles.statusText, status === "Fail"? { color:theme.failColor }: status === "Success" ? { color:theme.successColor }: { color:theme.warningColor }]}>{status}</Text>
+        <Text style={styles.statusText}>{statusMessage}</Text>
+        <Text style={styles.messageText}>{message}</Text>
+    </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    dateText: {
+        color: theme.blackText,
+        fontSize: 18,
+        backgroundColor: theme.primaryBlue,
+        paddingHorizontal:20,
+        marginBottom: 5
+    },
+    messageText: {
+        color: theme.greyText,
+        fontSize: 14,
+        paddingHorizontal:20
+    },
+    statusText: {
+        color: theme.blackText,
+        fontSize: 16,
+        paddingHorizontal:20,
+    },
+    card: {
+        marginBottom: 20
+    }
+})
--- app/notifications/index.tsx
+++ app/notifications/index.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NotificationMessageComponent } from '../components/notificationMessageComponent';
 
 export default function Notifications() {
   return (
     <View style={styles.container}>
-      <Text>This is Notification screen</Text>
+      <NotificationMessageComponent date='25 Nov 2024' status='Fail' statusMessage='missing documents' message='Submit the required documents' />
+      <NotificationMessageComponent date='25 Nov 2024' status='Success' statusMessage='Ready document' message='Your document is ready please come t the facility and collect the document' />
+      <NotificationMessageComponent date='25 Nov 2024' status='Warning' statusMessage='Come to the facility to address this issue' message='Some security measure must be done before processding with the rest procedures' />
+      <NotificationMessageComponent date='25 Nov 2024' status='Fail' statusMessage='missing documents' message='Submit the required documents' />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +18,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  
+    paddingTop: 10,
+    
   },
 });
