cfb5664c462b915e786d11b0b4d0a96e80729d01
Eyad Aimen <70057337+EyadAimen@users.noreply.github.com>
2024-11-26 19:34:23 +0800

    test

---
+++ firebaseConfig.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCczSWHZw9-5MrberczAOE0ePk9bfIJQEU",
+  authDomain: "digital-issuance-54ab9.firebaseapp.com",
+  projectId: "digital-issuance-54ab9",
+  storageBucket: "digital-issuance-54ab9.firebasestorage.app",
+  messagingSenderId: "1072963974905",
+  appId: "1:1072963974905:web:60dc4d661b6ada72ebd428",
+  measurementId: "G-CDMB7J1N0M"
+};
+
+// Initialize Firebase
+const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+const analytics = getAnalytics(FIREBASE_APP);
