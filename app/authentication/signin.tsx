7839a4fdda7d070c23571475afd3366395c10662
Muaz02 <167197216+MoazJalal@users.noreply.github.com>
2024-11-28 08:00:39 +0800

    Revert "Merge branch 'develop' of https://github.com/EyadAimen/Digital-issuance into develop"
    This reverts commit 136bfee3b6eaa41b704a994d1ce3b50bd5693fa9, reversing
    changes made to 2ef725d398b9bcca0aaf3ef90a40a0e0267ad2c8.

--- app/authentication/signin.tsx
+++ app/authentication/signin.tsx
@@ -1,121 +1,24 @@
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { KeyboardAvoidingView, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
-import { theme } from '../../theme';
-import { FIREBASE_AUTH } from '../../firebaseConfig';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { useState } from 'react';
-import { fontStyles } from '../../fonts';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default function SginIn() {
-  // Handle the input fields
-  const [email, setEmail] = useState<string>('')
-  const [password, setPassword] = useState<string>('')
-  
-
-  const auth = FIREBASE_AUTH;
-
-  const handleSignIn = async() => {
-    try {
-      const response = await signInWithEmailAndPassword(auth, email, password).then(()=>{alert("signed in")});
-      
-    } catch(e: any) {
-      alert('Sign in failed' + e.message);
-    }
-  }
+export function SginIn() {
   return (
     <View style={styles.container}>
-      <KeyboardAvoidingView behavior='position' >
-            {/* Email */}
-            <View style={styles.inputContianer}>
-              <Text>Email<Text style={{color:theme.failColor}}>*</Text></Text>
-              <TextInput
-                value={email}
-                style={styles.textInput}
-                onChangeText={setEmail}
-                placeholder="E.g muhammad@gmail.com"
-              />
-            </View>
-            
-            {/* Passowrd */}
-            <View style={styles.inputContianer}>
-              <Text>Password<Text style={{color:theme.failColor}}>*</Text></Text>
-              <TextInput
-                value={password}
-                style={styles.textInput}
-                onChangeText={setPassword}
-                placeholder="E.g Password123"
-                secureTextEntry={true}
-              />
-            </View>
-
-          <Pressable style={styles.buttonStyle} onPress={handleSignIn}>
-            <Text style={[fontStyles.buttonLabels, {color:theme.whiteColor}]}>Sign in</Text>
-          </Pressable>
-
-          <View style={styles.signUpBox}>
-          <Text>Don't have an account? </Text>
-          <Link href={"authentication/signup"} style={styles.signUpText}>Sign up</Link>
-          </View>
-          </KeyboardAvoidingView>
-          <StatusBar style="auto" />
-        </View>
+        <Link href={"/authentication/signup"}>
+            go to sign up
+        </Link>
+      <Text>This is sign in screen</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: theme.whiteColor,
-    paddingVertical: 56,
-    paddingHorizontal: 18,
-    gap: 48,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  inputsContainer: {
-    marginTop: 32,
-    gap:12,
-  },
-  inputContianer: {
-    display: "flex",
-    gap: 8,
-    alignItems: "flex-start",
-    justifyContent: "flex-start",
-  },
-  textInput: {    
-    borderColor: theme.grey2Text,
-    borderWidth: 1,
-    alignSelf: "stretch",
-    padding: 10,
-    fontSize: 16,
-    marginBottom: 12,
-    borderRadius: 6,
-  },
-  buttonStyle: {
-    backgroundColor: theme.primaryBlue,
-    paddingVertical: 16,
-    paddingHorizontal: 10,
-    alignItems: "center",
-    borderRadius: 6,
-    marginTop: 20,
-  },
-
-  label: {
-    position: 'absolute',
-    backgroundColor: 'white',
-    left: 22,
-    top: 8,
-    zIndex: 999,
-    paddingHorizontal: 8,
-    fontSize: 14,
-  },
-  placeholderStyle: {
-    fontSize: 16,
-  },
-  signUpBox: {
-    flexDirection: "row",
-    marginTop: 10
-  },
-  signUpText: {
-    color: theme.primaryBlue,
-  }
 });
