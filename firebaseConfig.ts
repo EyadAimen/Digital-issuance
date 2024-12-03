75adab82277ee9c6737a416295aa39842d8ca2c0
Eyad Aimen <70057337+EyadAimen@users.noreply.github.com>
2024-12-03 16:43:25 +0800

    saved user state between sessions

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,8 +1,9 @@
 
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+import { getAuth, initializeAuth, getReactNativePersistence  } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCczSWHZw9-5MrberczAOE0ePk9bfIJQEU",
@@ -16,6 +17,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const FIREBASE_APP = initializeApp(firebaseConfig);
-export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
 export const FIREBASE_DB = getFirestore(FIREBASE_APP);
 const analytics = getAnalytics(FIREBASE_APP);
